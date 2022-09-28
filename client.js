b2e21970e64d474530d905f38b26c6e61699cea6
curtis-wils0n <curtis.wils0n@protonmail.com>
2022-09-28 19:24:30 +0000

    Created client connection function and play launch-file

---
+++ client.js
@@ -0,0 +1,18 @@
+// establishes a connection with the game server4
+const net = require('net');
+const connect = function () {
+  const conn = net.createConnection({
+    host: 'localhost',
+    port: 50541
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+}
+
+module.exports = connect;
